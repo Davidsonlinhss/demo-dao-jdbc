b3b3d751967aac42ed7d5a1a1c98af02a176cd80
Davidson Linhares <davidson.linhares@outlook.com>
2024-09-17 22:36:08 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,7 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(12, "John Doe", "john@gmail.com", new Date(), 300.0, obj);
-        System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
