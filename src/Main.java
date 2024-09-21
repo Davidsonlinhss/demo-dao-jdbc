abccff2ed5fc0a6a51bc3adb12d3d432c2767b72
Davidson Linhares <davidson.linhares@outlook.com>
2024-09-21 09:45:54 -0300

    update implementation

--- src/Main.java
+++ src/Main.java
@@ -39,5 +39,12 @@ public class Main {
         Seller newSeller = new Seller(null, "Davidson", "Davidson@gmail.com", new Date(), 4000.0, department);
        sellerDao.insert(newSeller);
        System.out.println("Inserted! New id = " + newSeller.getId());
+
+       System.out.println("=== TEST 5: seller update ===");
+       seller = sellerDao.getById(1); // selecionando qual vendedor por Id
+       seller.setName("Marcelo");
+        seller.setEmail("Marcelo@gmail.com"); // setando o novo nomeseller.
+       sellerDao.update(seller);
+       System.out.println("Updated! New id = " + seller.getId());
     }
 }
